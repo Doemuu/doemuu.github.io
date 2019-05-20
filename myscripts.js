4b01dbddba7ef8776a89707a9a89d1e953f142a0
Doemuu <43069409+Doemuu@users.noreply.github.com>
2019-05-21 00:06:25 +0200

    Create Contact

--- myscripts.js
+++ myscripts.js
@@ -15,5 +15,69 @@ function makeParagraph() {
   return mainText;
 }
 
+function onContactButtonClicked() {
+  var b = false; //boolean to check all inputs
+  var lastNamebool = false;
+  var firstNameBool = false;
+  var messageBool = false;
+  var lastName = document.getElementById("lastNameText").value;
+  var firstName = document.getElementById("firstNameText").value;
+  var message = document.getElementById("messageText").value;
+  if (lastName == "") {
+    lastName = "wack";
+    //lastName is null
+  } else {
+    lastNamebool = true;
+  }
+  if (firstName == "") {
+    //firstName is null
+    firstName = "wack";
+  } else {
+    firstNameBool = true;
+  }
+  if (message == "") {
+    //message is null
+    message = "wack";
+  } else {
+    messageBool = true;
+  }
+  if (lastNamebool) {
+    //when lastName is correct
+    if (firstNameBool) {
+      //when firstName is correct
+      if (messageBool) {
+        //when message is correct
+        b = true;
+      } else {
+        //when message is wack
+        document.getElementById("messageText").value = "";
+      }
+    } else {
+      //when firstName is wack
+      document.getElementById("firstNameText").value = "";
+    }
+  } else {
+    //when lastname is wack
+    document.getElementById("lastNameText").value = "";
+  }
+
+  if (b) {
+    document.getElementById("lol").innerHTML = message;
+    Email.send({
+      SecureToken: "744700c4-a70f-4911-8076-3ccd29a6a0c1",
+      To: 'dominik.berger17@outlook.com',
+      From: "doemu@outlook.com",
+      Subject: firstName + " " + lastName + " will dich kontaktieren",
+      Body: message
+    }).then(
+      message => alert(message)
+    );
+  }
+
+  document.getElementById("lastNameText").value = "";
+  document.getElementById("firstNameText").value = "";
+  document.getElementById("messageText").value = "";
+}
+
 //build
 document.getElementById("demo").innerHTML = makeParagraph();
