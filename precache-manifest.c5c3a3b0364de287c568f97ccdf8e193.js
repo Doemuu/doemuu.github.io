0e5cb413e3310fa8a8872b0a0304669b9225ac5b
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-05-14 12:12:49 +0200

    Run Build

---
+++ precache-manifest.c5c3a3b0364de287c568f97ccdf8e193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2665f406673fdb976d6f2de853ff83c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2cf9b9877509e480280",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43d0a69c0c271befbef3",
+    "url": "/static/js/2.c6d0dfa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6d0dfa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2cf9b9877509e480280",
+    "url": "/static/js/main.29750eb0.chunk.js"
+  },
+  {
+    "revision": "b5142068811823455baf",
+    "url": "/static/js/runtime-main.79a37825.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
